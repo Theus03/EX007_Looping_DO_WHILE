6ded0c569b6cf2b7f9590dc4bbd351a887550890
DelevoperTheus <Theus03>
2020-06-21 14:05:09 -0300

    Ex007_Looping_Do_While

---
+++ Ex007_LoopingDoWhile.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercícios;
+
+/**
+ *
+ * @author Matheus
+ */
+import java.util.Scanner;
+public class Ex007_LoopingDoWhile {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite a temperatura em Celsius:");
+        double c = entrada.nextDouble();
+        int i = 1;
+        do{
+            double f = (9 * c + 160)/5;
+            System.out.println(f);
+            i++;
+            c = c+10;
+        }
+        while(i<=10);
+    }
+}
